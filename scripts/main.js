419bb07b377a16291dee6dcc84bd4400b3e7a6c2
Corey Jackson <corey.jackson@jax.tech>
2025-09-09 14:48:30 -0400

    calc storage v1

---
+++ scripts/main.js
@@ -0,0 +1,88 @@
+const strDBName = "TestDatabase";
+
+function checkIndexedDBSupport()
+{
+    if (window.indexedDB)
+    {
+        console.log("Indexed DB supported");
+        document.getElementById("btnCreateDB").disabled = false;
+        document.getElementById("btnCreateObjectStore").disabled = false;
+        document.getElementById("btnCalcStorage").disabled = false;
+    }
+    else
+    {
+        alert("Sorry! Your browser does not support IndexedDB");
+    }
+}
+
+function createDatabase()
+{
+    // Let us open our database
+    const request = window.indexedDB.open(strDBName, 1);
+
+    request.onsuccess = (event) => {
+        console.log("database created successfully");
+        document.getElementById("btnCreateObjectStore").disabled = false;
+        //const db = event.target.result;
+    };
+
+    request.onerror = (event) => {
+        console.log("error creating database");
+    };
+}
+
+function createObjectStore()
+{
+    const request = indexedDB.open(strDBName, 3); // Version 2
+
+    request.onupgradeneeded = function(event) {
+        const db = event.target.result;
+
+        const customerData = [
+            { id: "1", name: "Bill", age: 35, email: "bill@company.com" },
+            { id: "2", name: "Donna", age: 32, email: "donna@home.org" },
+        ];
+
+        // This is the correct place to create or delete object stores
+        if (!db.objectStoreNames.contains('MainStore')) {
+            const objectStore = db.createObjectStore('MainStore', { keyPath: 'id' });
+            objectStore.createIndex("name", "name", { unique: false });
+            objectStore.createIndex("email", "email", { unique: true });
+
+            // Use transaction oncomplete to make sure the objectStore creation is
+            // finished before adding data into it.
+            objectStore.transaction.oncomplete = (event) => {
+                // Store values in the newly created objectStore.
+                const customerObjectStore = db
+                    .transaction("MainStore", "readwrite")
+                    .objectStore("MainStore");
+                        customerData.forEach((customer) => {
+                        customerObjectStore.add(customer);
+                });
+            };
+        }
+        // You can also add or modify indexes here
+    };
+}
+
+async function checkStorage()
+{
+    console.log("Checking Storage");
+    if (navigator.storage && navigator.storage.estimate) {
+        const objStorage = await navigator.storage.estimate();
+        // .usage -> Number of bytes used.
+        console.log("quota" + objStorage.usage);
+        document.getElementById("storageQuota").innerText = objStorage.usage.toString();
+
+        // .quota -> Maximum number of bytes available.
+        document.getElementById("storageBytesAvailable").innerText = objStorage.quota.toString();
+
+        const percentageUsed = (objStorage.usage / objStorage.quota) * 100;
+        console.log(`You've used ${percentageUsed}% of the available storage.`);
+        document.getElementById("storagePctUsed").innerText = percentageUsed.toString();
+
+        const remaining = objStorage.quota - objStorage.usage;
+        document.getElementById("storageRemaining").innerHTML = remaining.toString();
+        console.log(`You can write up to ${remaining} more bytes.`);
+    }
+}
